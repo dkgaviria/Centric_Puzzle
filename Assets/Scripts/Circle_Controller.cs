b87e84c7df1d2ba5054d12eeea0d3259ccc11699
dkgaviria <31330982+dkgaviria@users.noreply.github.com>
2017-08-24 19:44:51 -0700

    added project
    added project to repo

---
+++ Assets/Scripts/Circle_Controller.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Circle_Controller : MonoBehaviour {
+    public GameObject circle;
+    public bool correct;
+    public float mouseY;
+    public float threshold=0;
+	// Use this for initialization
+	void Start () {
+        circle.transform.Rotate(new Vector3(0, 22.5f * Random.Range(2,25), 0));
+
+    }
+    private void OnMouseDown()
+    {
+        if(this.gameObject.tag=="Circle")
+        {
+            
+            threshold = Input.mousePosition.y;
+        }
+        
+    }
+
+    private void OnMouseDrag()
+    {
+        mouseY = Input.mousePosition.y;
+       
+        if (threshold - mouseY >= 30)
+        {
+            threshold = mouseY;
+            circle.transform.Rotate(new Vector3(0,22.5f,0));
+            
+
+        }
+
+        else if(threshold - mouseY <= -30)
+        {
+            threshold = mouseY;
+            circle.transform.Rotate(new Vector3(0, -22.5f, 0));
+           
+
+        }
+        
+    }
+    private void OnMouseUp()
+    {
+
+        if (Mathf.RoundToInt( circle.transform.eulerAngles.y)==0)
+        {
+            correct = true;
+        }
+        else
+        {
+            correct = false;
+        }
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Win_Condition.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Win_Condition : MonoBehaviour {
+    public int correct { get; private set; }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+    private void Win_Condition.WinGame()
+    {
+        if (correct == 0)
+            {
+               MonoBehaviour.print("Good Work");
+            }
+        }
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
